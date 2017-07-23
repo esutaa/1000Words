b503dbcc1d391b5f6ec13b5dc636040d523a4c3f
Sergio Andrés <andres.sergio@icloud.com>
2017-07-22 21:43:06 -0400

    Added Login/Register fragments

--- app/src/main/java/edu/fsu/cs/mobile/onethousandwords/LoginFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/onethousandwords/LoginFragment.java
@@ -2,16 +2,25 @@ package edu.fsu.cs.mobile.onethousandwords;
 
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+
+    private EditText EMAIL;
+    private EditText PASSWORD;
+    private Button LOGIN;
+    private TextView CREATE;
 
 
     public LoginFragment() {
@@ -23,7 +32,41 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        EMAIL = (EditText) rootView.findViewById(R.id.email);
+        PASSWORD = (EditText) rootView.findViewById(R.id.password);
+        LOGIN = (Button) rootView.findViewById(R.id.login_btn);
+
+        LOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DrawingFragment drawingFragment = new DrawingFragment();
+                String tag = DrawingFragment.class.getCanonicalName();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_frame, drawingFragment, tag)
+                        .addToBackStack("login")
+                        .commit();
+            }
+        });
+
+        CREATE = (TextView) rootView.findViewById(R.id.create_account_txt);
+        CREATE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RegisterFragment registerFragment = new RegisterFragment();
+                String tag = RegisterFragment.class.getCanonicalName();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_frame, registerFragment, tag)
+                        .addToBackStack("register")
+                        .commit();
+            }
+        });
+
+
+        return rootView;
     }
 
 }
--- app/src/main/java/edu/fsu/cs/mobile/onethousandwords/RegisterFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/onethousandwords/RegisterFragment.java
@@ -3,15 +3,25 @@ package edu.fsu.cs.mobile.onethousandwords;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.print.PrintHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+
+    private EditText NAME;
+    private EditText EMAIL;
+    private EditText PASSWORD;
+    private Button CREATE_ACCOUNT;
+    private TextView MEMBER_LOGIN;
 
 
     public RegisterFragment() {
@@ -23,7 +33,41 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_register, container, false);
+
+        NAME = (EditText) rootView.findViewById(R.id.name);
+        EMAIL = (EditText) rootView.findViewById(R.id.email);
+        PASSWORD = (EditText) rootView.findViewById(R.id.password);
+
+        CREATE_ACCOUNT = (Button) rootView.findViewById(R.id.create_account_btn);
+        CREATE_ACCOUNT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DrawingFragment drawingFragment = new DrawingFragment();
+                String tag = DrawingFragment.class.getCanonicalName();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_frame, drawingFragment, tag)
+                        .addToBackStack("register")
+                        .commit();
+            }
+        });
+
+        MEMBER_LOGIN = (TextView) rootView.findViewById(R.id.already_member_login);
+        MEMBER_LOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LoginFragment loginFragment = new LoginFragment();
+                String tag = LoginFragment.class.getCanonicalName();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_frame, loginFragment, tag)
+                        .addToBackStack("login")
+                        .commit();
+            }
+        });
+
+        return rootView;
     }
 
 }
